9dfa49800f72691c953d66ed3090723e3b52c6c3
Alina Zacaria <azacaria@luc.edu>
2024-10-13 23:31:58 -0500

    reimplemented tests to match updated main

--- src/test/scala/hellotest/StackTest.scala
+++ src/test/scala/hellotest/StackTest.scala
@@ -9,9 +9,9 @@ class MainSpec extends AnyFlatSpec {
 
   "The word cloud generator" should "correctly count word frequencies" in {
     val words = Seq("hello", "world", "hello", "scala", "test", "hello", "world")
-    val queue = new CircularFifoQueue[String]
+    val queue = new CircularFifoQueue[String]  // specify the queue size
 
-    words.foreach(queue.add)
+    words.foreach(queue.add.nn)
 
     val wordCount = mutable.Map[String, Int]()
     queue.forEach { w =>
@@ -26,13 +26,13 @@ class MainSpec extends AnyFlatSpec {
 
   it should "ignore words shorter than the specified length" in {
     val words = Seq("hi", "there", "hello", "world")
-    val queue = new CircularFifoQueue[String]
+    val queue = new CircularFifoQueue[String]  // specify the queue size
 
     words.foreach(queue.add)
 
     val wordCount = mutable.Map[String, Int]()
-    queue.forEach { w =>
-      if (w.nn.length >= 5) {
+    queue.forEach.nn { w =>
+      if (w.nn.length >= 5) { // Use the specified minimum length condition
         wordCount(w.nn) = wordCount.getOrElse(w.nn, 0) + 1
       }
     }
@@ -41,7 +41,7 @@ class MainSpec extends AnyFlatSpec {
   }
 
   it should "handle empty input gracefully" in {
-    val queue = new CircularFifoQueue[String]
+    val queue = new CircularFifoQueue[String]  // specify the queue size
     val wordCount = mutable.Map[String, Int]()
 
     queue.forEach { w =>
@@ -49,5 +49,26 @@ class MainSpec extends AnyFlatSpec {
     }
 
     wordCount shouldBe empty
+  }
+
+  it should "consider minimum frequency for word inclusion" in {
+    val words = Seq("hello", "world", "hello", "hello", "world", "test")
+    val queue = new CircularFifoQueue[String]  // specify the queue size
+
+    words.foreach(queue.add)
+
+    val wordCount = mutable.Map[String, Int]()
+    queue.forEach { w =>
+      wordCount(w.nn) = wordCount.getOrElse(w.nn, 0) + 1
+    }
+
+    // Set a minimum frequency threshold
+    val minFrequency = 2
+    val filteredWords = wordCount.filter { case (_, count) => count >= minFrequency }
+
+    val sortedWords = filteredWords.toSeq.sortBy { case (word, count) => (-count, word) }
+    val topWords = sortedWords.take(3)
+
+    topWords should contain theSameElementsAs Seq(("hello", 3), ("world", 2))
   }
 }
